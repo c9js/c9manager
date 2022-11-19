867d81dc755581379273ed32697899c3b346ac50
Sergei <email@email.email>
2022-11-19 09:48:47 +0000

    1.0.34

--- c9start.bat
+++ c9start.bat
@@ -1,1 +1,1 @@
-docker run -itv "//var/run/docker.sock:/var/run/docker.sock" -e "CD=%CD%" --privileged --rm c9js/c9start:1.0.33
+docker run -itv "//var/run/docker.sock:/var/run/docker.sock" -e "CD=%CD%" --privileged --rm c9js/c9start:1.0.34
