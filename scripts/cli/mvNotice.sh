cf8da65c8055eee8147054e6e611f8310250e114
Sergei <email@email.email>
2023-02-02 11:37:00 +0000

    edit Notice

--- scripts/cli/mvNotice.sh
+++ scripts/cli/mvNotice.sh
@@ -8,20 +8,20 @@ modelView:Notice() { case "$1" in
 #┌──────────────────────────────┐
 #│ Проверяет список уведомлений │
 #└──────────────────────────────┘
-    'fatal_error') modelView:Notice 'run' "$2" "${FATAL_LIST[@]}"   ;; # Список фатальных ошибок
-    'error')       modelView:Notice 'run' "$2" "${ERROR_LIST[@]}"   ;; # Список ошибок
-    'warning')     modelView:Notice 'run' "$2" "${WARNING_LIST[@]}" ;; # Список предупреждений
-    'info')        modelView:Notice 'run' "$2" "${INFO_LIST[@]}"    ;; # Список информационных сообщений
+    'fatal_error') modelView:Notice 'run' "$1" "$2" "${FATAL_LIST[@]}"   ;; # Список фатальных ошибок
+    'error')       modelView:Notice 'run' "$1" "$2" "${ERROR_LIST[@]}"   ;; # Список ошибок
+    'warning')     modelView:Notice 'run' "$1" "$2" "${WARNING_LIST[@]}" ;; # Список предупреждений
+    'info')        modelView:Notice 'run' "$1" "$2" "${INFO_LIST[@]}"    ;; # Список информационных сообщений
     
 #┌───────────────────────────────────┐
 #│ Проверяет список всех уведомлений │
 #└───────────────────────────────────┘
     'check')
     # Проверяем списки уведомлений
-        modelView:Notice 'run' "$2" "${FATAL_LIST[@]}"   && return 0 # Фатальные ошибки
-        modelView:Notice 'run' "$2" "${ERROR_LIST[@]}"   && return 0 # Ошибки
-        modelView:Notice 'run' "$2" "${WARNING_LIST[@]}" && return 0 # Предупреждения
-        modelView:Notice 'run' "$2" "${INFO_LIST[@]}"    && return 0 # Информационные сообщения
+        modelView:Notice 'run' 'fatal_error' "$2" "${FATAL_LIST[@]}"   && return 0 # Фатальные ошибки
+        modelView:Notice 'run' 'error'       "$2" "${ERROR_LIST[@]}"   && return 0 # Ошибки
+        modelView:Notice 'run' 'warning'     "$2" "${WARNING_LIST[@]}" && return 0 # Предупреждения
+        modelView:Notice 'run' 'info'        "$2" "${INFO_LIST[@]}"    && return 0 # Информационные сообщения
         
     # Уведомления не найдены
         return 1
@@ -32,13 +32,14 @@ modelView:Notice() { case "$1" in
 #└─────────────────────────┘
     'run')
     # Список аргументов
-        local command="$2" # Текущая команда
+        local notice="$2"  # Тип уведомления
+        local command="$3" # Текущая команда
         
     # Локальные переменные
         local i
         
     # Проходим по списку команд
-        for ((i = 3; i <= $#; i++)); do
+        for ((i = 4; i <= $#; i++)); do
         # Проверяем текущую команду
             if [[ "$command" == '' || "$command" == "${!i}" ]]; then
             # Выполняем команду
@@ -47,7 +48,7 @@ modelView:Notice() { case "$1" in
                     NOTICE_COMMAND="${!i}"
                     
                 # Выводим уведомление на экран
-                    view:Notice "${!i}"
+                    view:Notice "$notice" "${!i}"
                     
                 # Уведомление найдено
                     return 0
